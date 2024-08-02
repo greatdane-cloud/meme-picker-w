1dc6d9ecf1f8497c82f15d60525f29d0e491c5ff
greatdane-cloud <vanderbyljeandre@gmail.com>
2024-08-02 15:14:41 +0200

    js file

--- index.js
+++ index.js
@@ -4,56 +4,39 @@ const emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
 const gifsOnlyOption = document.getElementById('gifs-only-option')
 
-
-
-/*
-Challenge:
-1. Set up an eventlistener which calls a new
-   function called "getMatchingCatsArray" when
-   the "Get Image" button is clicked.
-2. getMatchingCatsArray should save the value
-   of the checked radio input to a const and 
-   log out that const.
-*/
-
-
-
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
 getImageBtn.addEventListener('click', getMatchingCatsArray)
 
 function highlightCheckedOption(e){
-/*
-Challenge:
-1. Create an array of all items that have 
-   the "radio" class.
-2. Iterate over the array and remove the 
-   "highlight" class from each one.
-*/ 
-
     const radios = document.getElementsByClassName('radio')
-
     for (let radio of radios){
         radio.classList.remove('highlight')
     }
-
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
 
-function getMatchingCatsArray(){
-    if(document.querySelector('input[type="radio"]:checked)')){
+
+function getMatchingCatsArray(){     
+    if(document.querySelector('input[type="radio"]:checked')){
         const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
         const isGif = gifsOnlyOption.checked
-
+        
         const matchingCatsArray = catsData.filter(function(cat){
-                return cat.emotionTags.includes(selectedEmotion)
+            return cat.emotionTags.includes(selectedEmotion)
         })
-
+        
         console.log(matchingCatsArray)
-
-
-    }
-
+/*
+Challenge:
+1. Use the .filter() and .includes() methods to get 
+   an array of cats which have the selected emotion
+   in their emotionTags array. 
+2. Store this array in a const and log it out to check
+   it's working. Think: what would be a good name for the
+   const?
+*/  
+    }  
 }
 
 function getEmotionsArray(cats){
