cd4b3b9d472d00679c25033e9d6b923aee27b9d8
greatdane-cloud <vanderbyljeandre@gmail.com>
2024-08-03 18:41:58 +0200

    js fuc

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import { catsData } from '/data.js'
 const emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
 const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
-getImageBtn.addEventListener('click', getMatchingCatsArray)
+getImageBtn.addEventListener('click', renderCat)
 
 function highlightCheckedOption(e){
     const radios = document.getElementsByClassName('radio')
@@ -35,6 +37,25 @@ function getMatchingCatsArray(){
         })
         
           return (matchingCatsArray)
+
+function getSingleCatObject(){
+
+    const catsArray = getMatchingCatsArray()
+
+    if (catsArray.length === 1){
+        return catsArray[0]
+    } 
+    else {
+
+        const randomNumber = Math.floor(Math.random() * catsArray.length)
+        return catsArray[randomNumber]
+    }
+}
+
+function renderCat(){
+    getSingleCatObject()
+}
+
 /*
 Challenge:
 1. Use the .filter() and .includes() methods to get 
